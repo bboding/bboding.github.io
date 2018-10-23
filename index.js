bf04b2ca052e39bfad5512adc94da66881a7ee46
bokyeong <hawnie1004@naver.com>
2018-10-23 15:48:43 +0900

    기프티스타/니콘내콘 스크립트 분리

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const writeData = util.promisify(fs.writeFile)
 
 const spreadsheetId = '17vx2FXgG1Ylzt2SWjuRkIre_4O3dsb49q1SmT408fQo'
 
+function sleep() {
+  return new Promise(resolve =>
+    setTimeout(resolve, Math.random() * (2000 - 1000) + 500)
+  )
+}
+
+function askCode(rl) {
+  return new Promise(resolve => {
+    rl.question('Enter the code from that page here: ', code => {
+      resolve(code)
+    })
+  })
+}
+
 program
   .version('0.1.0')
   .command('giftistar')
@@ -130,9 +144,9 @@ program
         const { results } = res.data
         
         for (const result of results) {
-          if (result.coupon_count === 0) {
-            stockMoney = 0
-          } else stockMoney = result.stock_money
+          if (result.cupon_count === 0) {
+            couponStockMoney = 0
+          } else couponStockMoney = result.stock_money
           giftistarItems.push([
             result.objectId,
             brand.name,
@@ -142,16 +156,17 @@ program
             result.buy_price,
             `${result.price - result.discount_step2}`,
             result.price,
-            result.coupon_count,
-            stockMoney
+            result.cupon_count,
+            couponStockMoney
           ])
         }
+        console.log(brand.name)
         await sleep()
       }
 
       await sheets.spreadsheets.values.update({
         spreadsheetId,
-        range: 'A3:I99999',
+        range: 'giftistar!A3:J99999',
         valueInputOption: 'RAW',
         resource: {
           values: giftistarItems
@@ -160,276 +175,62 @@ program
 
       await sheets.spreadsheets.values.update({
         spreadsheetId,
-        range: 'A1',
+        range: 'giftistar!A1',
         valueInputOption: 'RAW',
         resource: {
           values: [
-            [`기프티스타 (업데이트된 날짜 ${moment().format('YY년 MM월 DD일')})`]
+            [`기프티스타 (시트 업데이트 ${moment().format('YY년 MM월 DD일 HH:MM')})`]
           ]
         }
       })
       console.log('dated')
 
-    } catch (error) {
-      console.log(error)
-      process.exit()
-    }
-  })
-
-program
-  .version('0.1.0')
-  .command('ncnc')
-  .option('-t, --token [token]', '토큰을 입력합니다.')
-  .action(async options => {
-    let { token } = options
-
-    try {
-      let ncncConItems = []
-      const axiosOptions = {
-        headers: {
-          Authorization: `Bear ${token}`
-        }
-      }
-      const response = await axios.get(
-        'http://localhost:3010/cms/con-category1s',
-        axiosOptions
-      )
-      const { conCategory1s } = response.data
-
-      for (const conCategory1 of conCategory1s) {
-        const response = await axios.get(
-          `http://localhost:3010/cms/con-category2s?conCategory1Id=${
-            conCategory1.id
-          }`,
-          axiosOptions
-        )
-        const { conCategory2s } = response.data
-
-        for (const conCategory2 of conCategory2s) {
-          const response = await axios.get(
-            `http://localhost:3010/cms/con-items?conCategory2Id=${
-              conCategory2.id
-            }`,
-            axiosOptions
-          )
-          const { conItems } = response.data
-          
-          for (const conItem of conItems) {
-            ncncConItems.push([
-              conItem.id,
-              conCategory2.name,
-              conItem.name,
-              conItem.originalPrice,
-              conItem.askingPrice,
-              conItem.ncSellingPrice,
-              conItem.sfSellingPrice
-            ])
-          }
-          sleepShort()
-        }
-      }
-
-      const content = await getData('credentials.json')
-      const credentials = JSON.parse(content)
-
-      const {
-        client_secret,
-        client_id,
-        redirect_uris
-      } = credentials.installed
-
-      const oAuth2Client = new google.auth.OAuth2(
-        client_id,
-        client_secret,
-        redirect_uris[0]
-      )
-
-      let tokenData
-      try {
-        tokenData = await getData(TOKEN_PATH)
-      } catch (err) {
-        const authUrl = oAuth2Client.generateAuthUrl({
-          access_type: 'offline',
-          scope: SCOPES
-        })
-        console.log('Authorizse this app by visiting this url:', authUrl)
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        })
-
-        const code = await askCode(rl)
-
-        const {tokens} = await oAuth2Client.getToken(code)
-
-        oAuth2Client.setCredentials(tokens)
-
-        await writeData(TOKEN_PATH, JSON.stringify(tokens))
-
-        console.log('Token stored to', TOKEN_PATH)
-
-        tokenData = await getData(TOKEN_PATH)
-      }
-
-      oAuth2Client.setCredentials(JSON.parse(tokenData))
-
-      console.log('onUpdate')
-      const sheets = google.sheets({
-        version: 'v4',
-        auth: oAuth2Client
-      })
-
-      await sheets.spreadsheets.values.update({
+      let giftistarDaily = []
+      const res = await sheets.spreadsheets.values.get({
         spreadsheetId,
-        range: 'sheet2!A3:G99999',
-        valueInputOption: 'RAW',
-        resource: {
-          values: ncncConItems
-        }
+        range: 'giftistar!I1:J1'
       })
-
-      await sheets.spreadsheets.values.update({
-        spreadsheetId,
-        range: 'sheet2!A1',
-        valueInputOption: 'RAW',
-        resource: {
-          values: [
-            [`니콘내콘 (업데이트된 날짜 ${moment().format('YY년 MM월 DD일')})`]
-          ]
-        }
-      })
-      console.log('dated')
-
-    } catch (error) {
-      console.log(error)
-      process.exit()
-    }
-  })
-program.parse(process.argv)
-
-program
-  .version('0.1.0')
-  .command('sheet3')
-  .option('-t, --token [token]', '토큰을 입력합니다.')
-  .action(async options => {
-    let { token } = options
-
-    try {
-      const content = await getData('credentials.json')
-      const credentials = JSON.parse(content)
-
-      const {
-        client_secret,
-        client_id,
-        redirect_uris
-      } = credentials.installed
-
-      const oAuth2Client = new google.auth.OAuth2(
-        client_id,
-        client_secret,
-        redirect_uris[0]
-      )
-
-      let tokenData
-      try {
-        tokenData = await getData(TOKEN_PATH)
-      } catch (err) {
-        const authUrl = oAuth2Client.generateAuthUrl({
-          access_type: 'offline',
-          scope: SCOPES
-        })
-        console.log('Authorizse this app by visiting this url:', authUrl)
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        })
-
-        const code = await askCode(rl)
-
-        const {tokens} = await oAuth2Client.getToken(code)
-
-        oAuth2Client.setCredentials(tokens)
-
-        await writeData(TOKEN_PATH, JSON.stringify(tokens))
-
-        console.log('Token stored to', TOKEN_PATH)
-
-        tokenData = await getData(TOKEN_PATH)
-      }
-
-      oAuth2Client.setCredentials(JSON.parse(tokenData))
-
-      console.log('onUpdate')
-      const sheets = google.sheets({
-        version: 'v4',
-        auth: oAuth2Client
-      })
-
-      const axiosOptions = {
-        headers: {
-          Authorization: `Bear ${token}`
-        }
-      }
-      const response = await axios.get(
-        'http://localhost:3010/cms/con-items',
-        axiosOptions
-      )
-      const { conItems } = response.data
-      let sheet3ConItems = []
-      for (const conItem of conItems) {
-        sheet3ConItems.push([
-          conItem.id,
-          conItem.conCategory2.name,
-          conItem.name,
+      const rows = res.data.values
+      rows.map((row) => {
+        giftistarDaily.push([
+          moment().format('YY년 MM월 DD일 HH:MM'),
+          row[0],
+          row[1]
         ])
-      }
-      sleepShort()
-
+      });
       await sheets.spreadsheets.values.update({
         spreadsheetId,
-        range: 'sheet3!A3:C99999',
+        range: 'giftistarDaily!A8:C8',
         valueInputOption: 'RAW',
         resource: {
-          values: sheet3ConItems
+          values: giftistarDaily
         }
       })
 
+      let starbucksDaily = []
+      const res2 = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: 'giftistar!I4:J4',
+      })
+      const row2 = res2.data.values
+      row2.map((row) => {
+        starbucksDaily.push([
+          row[0],
+          row[1]
+        ])
+      })
       await sheets.spreadsheets.values.update({
         spreadsheetId,
-        range: 'sheet3!A1',
+        range: 'giftistarDaily!D8:E8',
         valueInputOption: 'RAW',
         resource: {
-          values: [
-            [`짝맞추기 (업데이트된 날짜 ${moment().format('YY년 MM월 DD일')})`]
-          ]
+          values: starbucksDaily
         }
       })
-      console.log('dated')
+
     } catch (error) {
       console.log(error)
-      process.exit()
     }
   })
 program.parse(process.argv)
 
-
-
-
-function sleep() {
-  return new Promise(resolve =>
-    setTimeout(resolve, Math.random() * (20000 - 10000) + 5000)
-  )
-}
-
-function sleepShort() {
-  return new Promise(resolve =>
-    setTimeout(resolve, Math.random() * (5000 - 1000) + 5000))
-}
-
-function askCode(rl) {
-  return new Promise(resolve => {
-    rl.question('Enter the code from that page here: ', code => {
-      resolve(code)
-    })
-  })
-}
---
+++ ncnc.js
@@ -0,0 +1,210 @@
+const moment = require('moment')
+const axios = require('axios')
+const fs = require('fs')
+const readline = require('readline')
+const { google } = require('googleapis')
+const program = require('commander')
+const util = require('util')
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+const TOKEN_PATH = 'token.json'
+
+const getData = util.promisify(fs.readFile)
+const writeData = util.promisify(fs.writeFile)
+
+const spreadsheetId = '17vx2FXgG1Ylzt2SWjuRkIre_4O3dsb49q1SmT408fQo'
+
+function sleepShort() {
+  return new Promise(resolve =>
+    setTimeout(resolve, Math.random() * (5000 - 1000) + 5000))
+}
+
+function askCode(rl) {
+  return new Promise(resolve => {
+    rl.question('Enter the code from that page here: ', code => {
+      resolve(code)
+    })
+  })
+}
+program
+  .version('0.1.0')
+  .command('ncnc')
+  .option('-t, --token [token]', '토큰을 입력합니다.')
+  .action(async options => {
+    let { token } = options
+
+    try {
+      const content = await getData('credentials.json')
+      const credentials = JSON.parse(content)
+
+      const {
+        client_secret,
+        client_id,
+        redirect_uris
+      } = credentials.installed
+
+      const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      )
+
+      let tokenData
+      try {
+        tokenData = await getData(TOKEN_PATH)
+      } catch (err) {
+        const authUrl = oAuth2Client.generateAuthUrl({
+          access_type: 'offline',
+          scope: SCOPES
+        })
+        console.log('Authorizse this app by visiting this url:', authUrl)
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        })
+
+        const code = await askCode(rl)
+
+        const {tokens} = await oAuth2Client.getToken(code)
+
+        oAuth2Client.setCredentials(tokens)
+
+        await writeData(TOKEN_PATH, JSON.stringify(tokens))
+
+        console.log('Token stored to', TOKEN_PATH)
+
+        tokenData = await getData(TOKEN_PATH)
+      }
+
+      oAuth2Client.setCredentials(JSON.parse(tokenData))
+
+      console.log('onUpdate')
+      const sheets = google.sheets({
+        version: 'v4',
+        auth: oAuth2Client
+      })
+
+      let ncncConItems = []
+      const axiosOptions = {
+        headers: {
+          Authorization: `Bear ${token}`
+        }
+      }
+
+      await sheets.spreadsheets.values.clear({
+        spreadsheetId,
+        range: 'ncnc!A3:G99999'
+      })
+
+      const response = await axios.get(
+        'http://localhost:3010/cms/con-items',
+        axiosOptions
+      )
+      const { conItems } = response.data
+      
+      for (const conItem of conItems) {
+        ncncConItems.push([
+          conItem.id,
+          conItem.conCategory2.name,
+          conItem.name,
+          conItem.originalPrice,
+          conItem.askingPrice,
+          conItem.ncSellingPrice,
+          conItem.sfSellingPrice
+        ])
+      
+      sleepShort()
+      }
+
+      await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range: 'ncnc!A3:G99999',
+        valueInputOption: 'RAW',
+        resource: {
+          values: ncncConItems
+        }
+      })
+
+      await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range: 'ncnc!A1',
+        valueInputOption: 'RAW',
+        resource: {
+          values: [
+            [`니콘내콘/ 짝맞추기 (시트 업데이트 ${moment().format('YY년 MM월 DD일 HH:MM')})`]
+          ]
+        }
+      })
+      console.log('dated')
+
+      const sheet1 = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: 'A3:H99999'
+      })
+      const giftiDatas = sheet1.data.values
+
+      const sheet2 = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: 'ncnc!A3:H99999'
+      })
+      const ncncDatas = sheet2.data.values
+
+      let matchedItems = []
+      for (let i = 0; i < ncncDatas.length; i++) {
+        if(ncncDatas[i][7]) {
+          matchedItems.push([
+            ncncDatas[i][0],
+            ncncDatas[i][7]
+          ])
+        }
+      }
+
+      let list = []
+      for (let i = 0; i < ncncDatas.length; i++) {
+        for (let j = 0; j < matchedItems.length; j++) {
+          if (ncncDatas[i][0] === matchedItems[j][0]) {
+            for (let k = 0; k < giftiDatas.length; k++) {
+              if (giftiDatas[k][0] === matchedItems[j][1]) {
+                list.push([
+                  ncncDatas[i][1],
+                  ncncDatas[i][2],
+                  ncncDatas[i][3],
+                  giftiDatas[k][5],
+                  ncncDatas[i][4],
+                  giftiDatas[k][7],
+                  ncncDatas[i][5],
+                  ncncDatas[i][6]
+                ])
+              }
+            }
+          }
+        }
+      }
+
+      await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range: 'comparison!A3:H99999',
+        valueInputOption: 'RAW',
+        resource: {
+          values: list
+        }
+      })
+
+      await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range: 'comparison!A1',
+        valueInputOption: 'RAW',
+        resource: {
+          values: [
+            [`가격 비교 (시트 업데이트 ${moment().format('YY년 MM월 DD일 HH:MM')})`]
+          ]
+        }
+      })
+
+    } catch (error) {
+      console.log(error)
+      process.exit()
+    }
+  })
+program.parse(process.argv)
+
